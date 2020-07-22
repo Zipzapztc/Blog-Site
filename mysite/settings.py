c15f572c58230e76e5b8d531fef9ee6ad834e9ca
Zipzapztc <Zhuangtc_nb@foxmail.com>
2020-07-22 16:37:24 +0800

    2020/7/22 修改

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,13 +83,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '**************',
+        'USER': '**************',
+        'PASSWORD': '**************',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
